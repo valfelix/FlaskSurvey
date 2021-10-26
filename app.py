8a6822fb50672a0c464cc192b4dc55b860d2def3
Valeria Felix <76018199+valfelix@users.noreply.github.com>
2021-10-26 18:16:14 -0400

    Add updated app.py via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -7,7 +7,7 @@ app.config['SECRET_KEY'] = 'secret'
 
 debug = DebugToolbarExtension(app)
 
-RESPONSES = [] # As people answer questions, store their answers in this list.
+RESPONSES = 'responses' # As people answer questions, store their answers in this list.
 #  For example, at the end of the survey: ['Yes', 'No', 'Less than $10,000', 'Yes']
 
 @app.route('/')
@@ -18,21 +18,32 @@ def show_start():
     questions = survey.questions
     return render_template('start.html', title=title, instructions=instructions, questions=questions)
 
+# modify your start page so that clicking on the button fires off a POST request to a new route that will set session[“responses”] to an empty list
+@app.route("/begin", methods=["POST"])
+def start_survey():
+    """ Reset survey and redirect to the start of the survey. """
+
+    session[RESPONSES] = [] # set session[“responses”] to an empty list
+
+    return redirect("/questions/0")
+
 @app.route('/questions/<int:index>')
 def show_question(index):
     """ Show a form asking the current question, and listing the choices as radio buttons. """ 
-    if (RESPONSES is None):
+    responses = session.get(RESPONSES)
+
+    if (responses is None):
         # No answers have been appended to RESPONSES, trying to access question page before Start the Survey button has been clicked
         return redirect("/")
 
-    if (len(RESPONSES) == len(survey.questions)):
+    if (len(responses) == len(survey.questions)):
         # All answers have been appended to RESPONSES so they've answered all the questions - redirect to completion page
         return redirect("/complete")
 
-    if (len(RESPONSES) != index):
+    if (len(responses) != index):
         # Trying to access questions out of order so redirect them to the last unanswered question and flash a message telling them they’re trying to access an invalid question
         flash(f"Invalid question id: {index}.")
-        return redirect(f"/questions/{len(RESPONSES)}")
+        return redirect(f"/questions/{len(responses)}")
     
     q = survey.questions[index] # Access survey question through index since it's a list of instances inside the class
     choices = survey.questions[index].choices 
@@ -42,15 +53,17 @@ def show_question(index):
 def show_answer():
     """ When the user submits an answer, append this answer to responses list, and then redirect them to the next question. """
     choice = request.form['answer'] # get answer choice from form
-    RESPONSES.append(choice)
+    responses = session[RESPONSES]
+    responses.append(choice)
+    session[RESPONSES] = responses
 
-    if (len(RESPONSES) == len(survey.questions)):
+    if (len(responses) == len(survey.questions)):
         # Once all answers have been appended to RESPONSES, the length should equal length of survey, meaning they completed
         return redirect("/complete")
 
     else: 
         # If length of RESPONSES doesn't equal length of survey, they are not done and will be redirected to the remaining questions
-        return redirect(f"/questions/{len(RESPONSES)}")
+        return redirect(f"/questions/{len(responses)}")
     
 @app.route('/complete')
 def complete():
